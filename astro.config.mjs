6d3effd462ec81d3996e7db6aebedaa3d82e896c
JD <jesuspinolucena@gmail.com>
2024-02-27 22:40:18 +0100

    Add vercel Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "es"],
   },
+  output: "server",
+  adapter: vercel(),
 });
