9098a46caf630a5582847c01a8701172dd34a5df
JD <jesuspinolucena@gmail.com>
2024-03-02 00:21:02 +0100

    Format files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config"
+import react from "@astrojs/react"
+import tailwind from "@astrojs/tailwind"
+import vercel from "@astrojs/vercel/serverless"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en", "es"],
-  },
-  output: "server",
-  adapter: vercel(),
-});
+	integrations: [
+		react(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en", "es"],
+	},
+	output: "server",
+	adapter: vercel(),
+})
