8856c7a68772e9ab331866d4507ebd1c0546236d
JD <jesuspinolucena@gmail.com>
2024-03-02 00:19:50 +0100

    Eslint and Prettier working

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,49 +5,16 @@ process.env.ESLINT_TSCONFIG = "tsconfig.json"
  */
 module.exports = {
 	env: {
-		es2022: true,
 		node: true,
+		es2022: true,
 		browser: true,
 	},
-	extends: ["@antfu", "eslint-config-prettier"],
-	parser: "@typescript-eslint/parser",
+	extends: ["eslint:recommended", "plugin:astro/recommended"],
 	parserOptions: {
 		ecmaVersion: "latest",
 		sourceType: "module",
 	},
-	rules: {
-		"eol-last": "off",
-		"jsx-quotes": ["warn", "prefer-double"],
-		"quotes": ["warn", "double"],
-		"semi": ["warn", "never"],
-		"@stylistic/js/no-tabs": "off",
-		"@stylistic/ts/indent": "off",
-		"no-constant-binary-expression": "warn",
-		"no-debugger": "warn",
-		"no-extend-native": "off",
-		"no-trailing-spaces": "warn",
-		"space-before-function-paren": "off",
-		"antfu/if-newline": "off",
-		"antfu/top-level-function": "off",
-		"@stylistic/js/operator-linebreak": "off",
-		"@stylistic/ts/brace-style": "off",
-		"@stylistic/js/multiline-ternary": "off",
-		"n/prefer-global/process": "off",
-		"@stylistic/js/no-mixed-spaces-and-tabs": "off",
-		"no-unused-vars": "off",
-		"@typescript-eslint/no-unused-vars": "off",
-		"unused-imports/no-unused-imports": "warn",
-		"object-curly-newline": [
-			"warn",
-			{
-				consistent: true,
-				multiline: true,
-			},
-		],
-		"object-curly-spacing": ["warn", "always"],
-		"array-element-newline": ["warn", "consistent"],
-		"array-bracket-newline": ["warn", "consistent"],
-	},
+	rules: {},
 	overrides: [
 		{
 			files: ["*.astro"],
@@ -56,22 +23,29 @@ module.exports = {
 				parser: "@typescript-eslint/parser",
 				extraFileExtensions: [".astro"],
 			},
-			extends: ["plugin:astro/recommended", "plugin:astro/jsx-a11y-recommended"],
+			processor: "astro/client-side-ts",
+			rules: {},
+		},
+		{
+			files: ["*.ts"],
+			parser: "@typescript-eslint/parser",
+			extends: ["plugin:@typescript-eslint/recommended"],
 			rules: {
-				"astro/no-conflict-set-directives": "warn",
-				"astro/no-unused-define-vars-in-style": "warn",
-				"astro/no-unused-css-selector": "off",
-				"astro/prefer-class-list-directive": "warn",
-				"astro/semi": ["warn", "never"],
-				"astro/jsx-a11y/anchor-is-valid": "warn",
-				"@stylistic/ts/indent": "off",
+				"@typescript-eslint/no-unused-vars": [
+					"error",
+					{ argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" },
+				],
+				"@typescript-eslint/no-non-null-assertion": "off",
 			},
 		},
 		{
-			// Define the configuration for `<script>` tag.
-			// Script in `<script>` is assigned a virtual file name with the `.js` extension.
-			files: ["**/*.astro/*.js", "*.astro/*.js"],
+			files: ["*.tsx"],
 			parser: "@typescript-eslint/parser",
+			plugins: ["react", "@typescript-eslint"],
+			parserOptions: {
+				tsconfigRootDir: __dirname,
+				project: ["./tsconfig.json"],
+			},
 		},
 	],
 }
