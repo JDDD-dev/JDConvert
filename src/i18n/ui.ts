291a44222fa393be4e4e1507d0006fb61a2dd1a1
JD <jesuspinolucena@gmail.com>
2024-03-05 22:35:17 +0100

    i18n title and description

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -7,6 +7,8 @@ export const defaultLang = "en"
 
 export const ui = {
 	en: {
+		"index.title": "Convert now!",
+		"index.description": "Convert anything you need!",
 		"bytesToString.input": "Type your encoded message here.",
 		"bytesToString.selectLabel": "Select input format",
 		"bytesToString.hex": "Hexadecimal",
@@ -14,6 +16,8 @@ export const ui = {
 		"bytesToString.octal": "Octal",
 	},
 	es: {
+		"index.title": "Convierte ahora!",
+		"index.description": "Convierte cualquier cosa!",
 		"bytesToString.input": "Escribe tu mensaje cifrado aquí.",
 		"bytesToString.selectLabel": "Selecciona un formato",
 		"bytesToString.hex": "Hexadecimal",
