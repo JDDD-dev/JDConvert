b25de630e244d494ae76e0a3e9b0e9b5183a067b
JD <jesuspinolucena@gmail.com>
2024-03-12 20:24:37 +0100

    Add hover effect card component

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -6,7 +6,10 @@ const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElemen
 	({ className, ...props }, ref) => (
 		<div
 			ref={ref}
-			className={cn("rounded-lg border bg-card text-card-foreground shadow-sm", className)}
+			className={cn(
+				"hover:scale-103 rounded-lg border bg-card text-card-foreground shadow-sm transition ease-in-out hover:bg-cyan-950",
+				className
+			)}
 			{...props}
 		/>
 	)
