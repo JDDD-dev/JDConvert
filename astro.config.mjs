c9856513ee1e5b2ad0dce3f2b07c02252aaf250d
JDDD-dev <jesuspinolucena@gmail.com>
2024-04-04 16:08:52 +0200

    Prefect links by default

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
+	prefetch: {
+		prefetchAll: true,
+	},
 	integrations: [
 		react(),
 		tailwind({
