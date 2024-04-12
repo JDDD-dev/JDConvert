dc5b9ad3850613c6d740af1ab8914a44a88438bc
JDDD-dev <jesuspinolucena@gmail.com>
2024-04-12 20:44:20 +0200

    Fix PdfCreator logic

--- src/components/PdfCreator.tsx
+++ src/components/PdfCreator.tsx
@@ -3,12 +3,21 @@ import type { ui } from "@/i18n/ui"
 import { useDragAndDrop } from "@formkit/drag-and-drop/react"
 import { animations } from "@formkit/drag-and-drop"
 import { FileDown, Trash2 } from "lucide-react"
-import { PDFDocument, StandardFonts, rgb } from "pdf-lib"
+import {
+	PDFDocument,
+	PDFEmbeddedPage,
+	PDFImage,
+	PDFPage,
+	PageSizes,
+	StandardFonts,
+	rgb,
+} from "pdf-lib"
 import { startTransition, useEffect, useState } from "react"
 import { Document, pdfjs, Page } from "react-pdf"
-import { Button } from "./ui/button"
+import { Button } from "@/components/ui/button"
 import "react-pdf/dist/Page/TextLayer.css"
 import "react-pdf/dist/Page/AnnotationLayer.css"
+import { LoadingPdf } from "@/components/ui/loadingPdf"
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 	"pdfjs-dist/build/pdf.worker.min.js",
@@ -26,14 +35,14 @@ interface Pages {
 export default function PdfCreatorComponent({ lang }: Props) {
 	const t = useTranslations(lang)
 
+	const arrayPdf: File[] = []
 	const [pdfUrl, setPdfUrl] = useState("")
 	const [numPages, setNumPages] = useState(0)
 	const [pageNumber, setPageNumber] = useState(1)
 
-	function onDocumentLoadSuccess({ numPages }: Pages) {
-		setNumPages(numPages)
-		setPageNumber(1)
-	}
+	const [ref, pdfs, setPdfs] = useDragAndDrop<HTMLUListElement, File>(arrayPdf, {
+		plugins: [animations({ duration: 50 })],
+	})
 
 	const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
 		event.preventDefault()
@@ -55,8 +64,6 @@ export default function PdfCreatorComponent({ lang }: Props) {
 		}
 	}
 
-	const arrayPdf: File[] = []
-
 	const removeFromList = (pdf: File) => {
 		const index = pdfs.indexOf(pdf)
 
@@ -69,9 +76,9 @@ export default function PdfCreatorComponent({ lang }: Props) {
 		})
 	}
 
-	const [ref, pdfs, setPdfs] = useDragAndDrop<HTMLUListElement, File>(arrayPdf, {
-		plugins: [animations({ duration: 50 })],
-	})
+	const calculateScale = (page: PDFPage, embedded: PDFImage | PDFEmbeddedPage) => {
+		return Math.min(page.getWidth() / embedded.width, page.getHeight() / embedded.height)
+	}
 
 	async function createPdf() {
 		const pdfDoc = await PDFDocument.create()
@@ -80,7 +87,7 @@ export default function PdfCreatorComponent({ lang }: Props) {
 
 		if (pdfs.length === 0) {
 			const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman)
-			const page = pdfDoc.addPage()
+			const page = pdfDoc.addPage(PageSizes.A4)
 
 			const { width, height } = page.getSize()
 
@@ -98,35 +105,45 @@ export default function PdfCreatorComponent({ lang }: Props) {
 					const donorPdfBytes = await pdf.arrayBuffer()
 
 					const donorPdf = await PDFDocument.load(donorPdfBytes)
-					const copiedPages = await pdfDoc.copyPages(donorPdf, donorPdf.getPageIndices())
-					copiedPages.forEach((copiedPage) => {
-						pdfDoc.addPage(copiedPage)
-					})
+					await Promise.all(
+						donorPdf.getPages().map(async (page) => {
+							const newPage = pdfDoc.addPage()
+							const embedPage = await pdfDoc.embedPage(page)
+
+							const embedPageDims = embedPage.scale(calculateScale(newPage, embedPage))
+							newPage.drawPage(embedPage, {
+								...embedPageDims,
+								x: newPage.getWidth() / 2 - embedPageDims.width / 2,
+								y: newPage.getHeight() / 2 - embedPageDims.height / 2,
+							})
+							console.log(newPage.getWidth())
+						})
+					)
 				} else if (pdf.name.endsWith(".png")) {
 					const donorPngBytes = await pdf.arrayBuffer()
 
 					const pngImage = await pdfDoc.embedPng(donorPngBytes)
-					const page = pdfDoc.addPage()
+					const page = pdfDoc.addPage(PageSizes.A4)
 
-					const pngDims = pngImage.scale(1)
+					const pngDims = pngImage.scale(calculateScale(page, pngImage))
 
 					page.drawImage(pngImage, {
-						x: page.getWidth() / 2 - pngDims.width / 2 + 75,
-						y: page.getHeight() / 2 - pngDims.height,
+						x: page.getWidth() / 2 - pngDims.width / 2,
+						y: page.getHeight() / 2 - pngDims.height / 2,
 						width: pngDims.width,
 						height: pngDims.height,
 					})
 				} else {
 					const donorJpgBytes = await pdf.arrayBuffer()
 
-					const JpgImage = await pdfDoc.embedJpg(donorJpgBytes)
+					const jpgImage = await pdfDoc.embedJpg(donorJpgBytes)
 					const page = pdfDoc.addPage()
 
-					const jpgDims = JpgImage.scale(0.5)
+					const jpgDims = jpgImage.scale(calculateScale(page, jpgImage))
 
-					page.drawImage(JpgImage, {
-						x: page.getWidth() / 2 - jpgDims.width / 2 + 75,
-						y: page.getHeight() / 2 - jpgDims.height + 250,
+					page.drawImage(jpgImage, {
+						x: page.getWidth() / 2 - jpgDims.width / 2,
+						y: page.getHeight() / 2 - jpgDims.height / 2,
 						width: jpgDims.width,
 						height: jpgDims.height,
 					})
@@ -145,12 +162,10 @@ export default function PdfCreatorComponent({ lang }: Props) {
 		})
 	}
 
-	useEffect(() => {
-		const fetchCreatePdf = async () => {
-			await createPdf()
-		}
-		void fetchCreatePdf()
-	}, [pdfs])
+	function onDocumentLoadSuccess({ numPages }: Pages) {
+		setNumPages(numPages)
+		setPageNumber(1)
+	}
 
 	function changePage(offset: number) {
 		setPageNumber((prevPageNumber) => prevPageNumber + offset)
@@ -164,8 +179,15 @@ export default function PdfCreatorComponent({ lang }: Props) {
 		changePage(1)
 	}
 
+	useEffect(() => {
+		const fetchCreatePdf = async () => {
+			await createPdf()
+		}
+		void fetchCreatePdf()
+	}, [pdfs])
+
 	return (
-		<section className="flex h-[90dvh] w-full flex-col gap-40 p-14 pt-16 lg:flex-row">
+		<section className="flex h-[90dvh] w-full flex-col gap-10 p-14 pt-16 lg:flex-row">
 			<div
 				className="relative h-full w-full"
 				onDrop={handleDrop}
@@ -173,6 +195,7 @@ export default function PdfCreatorComponent({ lang }: Props) {
 			>
 				<input
 					type="file"
+					multiple
 					className="absolute left-0 top-0 z-20 h-full w-full opacity-0"
 					onChange={(value) => addFile(value)}
 				></input>
@@ -184,7 +207,7 @@ export default function PdfCreatorComponent({ lang }: Props) {
 				)}
 				<ul
 					ref={ref}
-					className="flex h-full w-full flex-col justify-center rounded-2xl border-2 border-sky-300 bg-slate-500 bg-opacity-40 p-4"
+					className="flex h-full w-full flex-col justify-center rounded-md border-2 border-sky-300 bg-slate-500 bg-opacity-40 p-4"
 				>
 					{pdfs.map((pdf) => (
 						<li className="z-30 w-full p-2" data-label={pdf} key={pdf.name}>
@@ -201,28 +224,33 @@ export default function PdfCreatorComponent({ lang }: Props) {
 					))}
 				</ul>
 			</div>
-			<div className="flex h-auto flex-col gap-4 pb-14">
+			<div className="relative flex h-full w-[595.28px] flex-col justify-center gap-4">
 				<Document
 					file={pdfUrl}
-					className="h-auto w-auto overflow-scroll"
+					className="h-full w-full overflow-x-hidden overflow-y-scroll rounded-md border-2 border-sky-300"
 					onLoadSuccess={onDocumentLoadSuccess}
+					loading={<LoadingPdf />}
+					noData={<LoadingPdf />}
 				>
 					<Page pageNumber={pageNumber}></Page>
 				</Document>
-				<div className="flex w-auto gap-3">
-					<p>
-						Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
-					</p>
-					<Button disabled={pageNumber <= 1} onClick={previousPage} className="w-full">
-						Previous
-					</Button>
-					<Button disabled={pageNumber >= numPages} onClick={nextPage} className="w-full">
-						Next
-					</Button>
-				</div>
-				<a download="JDCONVERT_GENERATED_PDF.pdf" href={pdfUrl}>
-					<Button className="w-full">Download</Button>
-				</a>
+				{pdfs.length !== 0 && (
+					<div className="absolute bottom-0 z-10 flex w-full gap-2 p-4 opacity-15 hover:opacity-80">
+						<Button disabled={pageNumber <= 1} onClick={previousPage} className="w-full bg-red-400">
+							Previous
+						</Button>
+						<Button
+							disabled={pageNumber >= numPages}
+							onClick={nextPage}
+							className="w-full bg-red-400"
+						>
+							Next
+						</Button>
+						<a download="JDCONVERT_GENERATED_PDF.pdf" href={pdfUrl}>
+							<Button className="w-full bg-red-400 hover:bg-red-400">Download</Button>
+						</a>
+					</div>
+				)}
 			</div>
 		</section>
 	)
