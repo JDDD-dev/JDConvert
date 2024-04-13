bad29aa8093886fa709cb435c55b0c9ab8fdd3fc
JDDD-dev <jesuspinolucena@gmail.com>
2024-04-13 16:00:24 +0200

    window size react hook

---
+++ src/lib/hooks.ts
@@ -0,0 +1,22 @@
+import { startTransition, useEffect, useState } from "react"
+
+const useSize = () => {
+	const [windowSize, setWindowSize] = useState([window.innerHeight, window.innerWidth])
+
+	useEffect(() => {
+		const windowSizeHandler = () => {
+			startTransition(() => {
+				setWindowSize([window.innerWidth, window.innerHeight])
+			})
+		}
+		window.addEventListener("resize", windowSizeHandler)
+
+		return () => {
+			window.removeEventListener("resize", windowSizeHandler)
+		}
+	}, [])
+
+	return windowSize
+}
+
+export default useSize
