c97c28c67f822aef1a08e882a58ba7a93fc0d935
JDDD-dev <jesuspinolucena@gmail.com>
2024-05-17 19:59:55 +0200

    set headers vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,19 @@ import vercel from "@astrojs/vercel/serverless"
 
 import sitemap from "@astrojs/sitemap"
 
+const viteServerConfig = {
+	name: "log-request-middleware",
+	configureServer(server) {
+		server.middlewares.use((req, res, next) => {
+			res.setHeader("Access-Control-Allow-Origin", "*")
+			res.setHeader("Access-Control-Allow-Methods", "GET")
+			res.setHeader("Cross-Origin-Opener-Policy", "same-origin")
+			res.setHeader("Cross-Origin-Embedder-Policy", "require-corp")
+			next()
+		})
+	},
+}
+
 // https://astro.build/config
 export default defineConfig({
 	prefetch: {
@@ -52,5 +65,6 @@ export default defineConfig({
 		build: {
 			chunkSizeWarningLimit: 900,
 		},
+		plugins: [viteServerConfig],
 	},
 })
